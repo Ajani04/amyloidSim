f42efe086d7d636bd830a94bfd3e6978f490eaff
Elvicharde <vicharde@gmail.com; freelanceel0@gmail.com>
2023-09-19 15:51:12 +0100

    Fixed the look of the Login page for better accessibility

--- client/src/pages/static pages/Login.js
+++ client/src/pages/static pages/Login.js
@@ -23,7 +23,7 @@ export default function Login() {
                     amyloidSim
                 </Link>
             </div>
-            <div className="col-5 mt-4 py-3 display-4 text-end mx-5">
+            <div className="col-6 mt-4 py-3 display-5 text-center mx-5">
                 Login to Your Account!
             </div>
             <div className="container-fluid mb-5">
